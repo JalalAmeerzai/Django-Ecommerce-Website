35c0aff2b73376c886e129ae0507c95eadecde1a
Jalal <Jalalx@msn.com>
2020-06-09 19:30:45 +0500

    update urls.py for shopapp by adding routing information in it and also update views.py for it

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='shop-home'),
+    path('about/', views.about, name='shop-about'),
+    path('contact/', views.contact, name='shop-contact'),
+    path('tracker/', views.tracker, name='shop-tracker'),
+    path('search/', views.search, name='shop-search'),
+    path('productview/', views.productView, name='shop-productView'),
+    path('checkout/', views.checkout, name='shop-checkout'),
 ]
--- shopapp/views.py
+++ shopapp/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shopapp/index.html')
+
+def about(request):
+    return render(request, 'shopapp/index.html')
+
+def contact(request):
+    return render(request, 'shopapp/index.html')
+
+def tracker(request):
+    return render(request, 'shopapp/index.html')
+
+def search(request):
+    return render(request, 'shopapp/index.html')
+
+def productView(request):
+    return render(request, 'shopapp/index.html')
+
+def checkout(request):
+    return render(request, 'shopapp/index.html')
