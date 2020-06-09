01ae91ecae4b462abde984f3cb9896e21295cbd8
Jalal <Jalalx@msn.com>
2020-06-09 20:20:51 +0500

    registed a model in admin.py of shopapp. update the model. and remigrated them and also created superuser

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     product_id = models.AutoField
     product_namme = models.CharField(max_length = 50)
     desc = models.CharField(max_length = 300)
