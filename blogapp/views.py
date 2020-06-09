7c592dec8a3cf0b9edc8262b4dd08ab12b0a4ff5
Jalal <Jalalx@msn.com>
2020-06-09 14:34:29 +0500

    added static files and loaded them. also chnaged settings.py for adding static folder directory

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Blog Home")
+    return render(request, 'blogapp/index.html')
