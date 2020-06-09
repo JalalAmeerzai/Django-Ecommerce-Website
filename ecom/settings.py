3650949858f6d44f9e7c3e7f3bb89280dfe7348c
Jalal <Jalalx@msn.com>
2020-06-09 19:59:49 +0500

    in  models.py file for shopapp, created a model product and migrated them to sqlite db. also changed settings.py for app line and changed its settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogapp',
-    'shopapp'
+    'blogapp.apps.BlogappConfig',
+    'shopapp.apps.ShopappConfig'
 ]
 
 MIDDLEWARE = [
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    product_id = models.AutoField
+    product_namme = models.CharField(max_length = 50)
+    desc = models.CharField(max_length = 300)
+    pub_date = models.DateField()
