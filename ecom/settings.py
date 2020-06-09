3c43ff39059dc5400071e6e5c66df1bbb57a8c13
Jalal <Jalalx@msn.com>
2020-06-09 23:08:58 +0500

    MEDIA MANAGEMENT: created a media folder in root for media files. also changed settings.py to add folder location. main project views.py also modified

--- ecom/settings.py
+++ ecom/settings.py
@@ -15,9 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
--- ecom/urls.py
+++ ecom/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shopapp.urls')),
     path('blogapp/', include('blogapp.urls')),
-    path('shopapp/', include('shopapp.urls')),
-]
+    path('shopapp/', include('shopapp.urls'))
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
