f9dab0c808320a9a62956e76c17fe7b58073c94d
Jalal <Jalalx@msn.com>
2020-06-11 00:37:32 +0500

    Dynamic Template: displayed db data on html page using some logics

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 def index(request):
-    return render(request, 'shopapp/index.html')
+    products = Product.objects.all() #This will fetch all the products
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'products': products}
+    return render(request, 'shopapp/index.html', params)
 
 def about(request):
     return render(request, 'shopapp/about.html')
