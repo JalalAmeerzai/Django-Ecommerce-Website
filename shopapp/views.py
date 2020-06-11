85aeccd4dc6e3ef53e62008bd491f28a6d643454
Jalal <Jalalx@msn.com>
2020-06-11 22:30:21 +0500

    Upgraded the home page with multiple product sliders categorywise

--- shopapp/views.py
+++ shopapp/views.py
@@ -4,10 +4,20 @@ from .models import Product
 from math import ceil
 
 def index(request):
-    products = Product.objects.all() #This will fetch all the products
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'products': products}
+    #products = Product.objects.all() #This will fetch all the products
+    #n = len(products)
+    #nSlides = n//4 + ceil((n/4)-(n//4))
+    #params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'products': products}
+    
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds}
     return render(request, 'shopapp/index.html', params)
 
 def about(request):
