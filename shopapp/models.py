bb2a1b5aada42d1f0f670f98bb21e80ab079825a
Jalal <Jalalx@msn.com>
2020-06-13 20:48:34 +0500

    Created contact us form and also created in Model in models.py and registered it in admin.py. Also updated contact in views.py.. also changed the cart logic in js to calculate no of items instead of number of products

--- shopapp/models.py
+++ shopapp/models.py
@@ -13,3 +13,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    sender_name = models.CharField(max_length = 50, null=True)
+    sender_email = models.CharField(max_length = 50, null=True)
+    sender_phone = models.CharField(max_length = 50, null=True)
+    msg_desc = models.CharField(max_length = 2000, null=True)
+
+    def __str__(self):
+        return self.sender_name + " - "+self.msg_desc[0:50]+"..." 
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 def index(request):
@@ -20,23 +20,54 @@ def index(request):
     params = {'allProds':allProds}
     return render(request, 'shopapp/index.html', params)
 
+
+
+
 def about(request):
     return render(request, 'shopapp/about.html')
 
+
+
+
 def contact(request):
-    return render(request, 'shopapp/contact.html')
+    params={"Message": -1}
+    if request.method == 'POST':
+        name = request.POST.get("name", None)
+        email = request.POST.get("email", None)
+        phone = request.POST.get("phone", None)
+        desc = request.POST.get("desc", None)
+
+        message = Contact(sender_name = name, sender_email = email, sender_phone =  phone, msg_desc = desc)
+        try:
+            message.save()
+            params["Message"] = 1
+        except Exception as identifier:
+            params["Message"] = 0
+    return render(request, 'shopapp/contact.html',params)
+
+
+
 
 def tracker(request):
     return render(request, 'shopapp/tracker.html')
 
+
+
+
 def search(request):
     return render(request, 'shopapp/search.html')
+
+
+
 
 def products(request, myid):
     #fetch the product using id
     prodView = Product.objects.filter(id=myid)
     params = {'product': prodView[0]}
     return render(request, 'shopapp/products.html', params)
+
+
+
 
 def checkout(request):
     return render(request, 'shopapp/checkout.html')
