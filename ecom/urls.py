fe6d4529b1303db55faaf0f73537c10117511eae
Jalal <Jalalx@msn.com>
2020-06-15 09:32:37 +0500

    added homepage to connect blogapp and shopapp in the main project

--- ecom/urls.py
+++ ecom/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shopapp.urls')),
     path('blogapp/', include('blogapp.urls')),
-    path('shopapp/', include('shopapp.urls'))
+    path('shopapp/', include('shopapp.urls')),
+    #path('', views.index, name="main-home"),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
